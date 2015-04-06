b3e93146d2b31aa32f535da4a017a30993921da9
datsjon13 <joncrisan13@gmail.com>
2015-04-06 09:38:24 -0400

    Updated topfile for Lab 6

--- Lab6/TopFile.vhd
+++ Lab6/TopFile.vhd
@@ -61,7 +61,7 @@ architecture Behavioral of TopFile is
 	component my_rege
 	generic (N: INTEGER:= 4);
 	port ( clock, resetn, sclr: in std_logic;
-	       E: in std_logic; -- sclr: Synchronous clear
+	       E: in std_logic;
 			 D: in std_logic_vector (N-1 downto 0);
 	       Q: out std_logic_vector (N-1 downto 0));
 	end component;	 
@@ -83,11 +83,12 @@ architecture Behavioral of TopFile is
 	  end component;
 
 signal E_IN0, E_OUT0, E_R1, E_R0,E_A0,E_G0: std_logic;
-signal G0, ALU0, mux0, mux1, mux2, mux3, BUS0,op0: std_logic_vector(3 downto 0); 
+signal G0, ALU0, mux0, mux1, mux2, mux3, BUS0,op0, leds0: std_logic_vector(3 downto 0); 
 signal SM0: std_logic_vector(1 downto 0); 
 signal sclrR: std_logic;
 begin
 sclrR <= '0';
+
 f0: my_rege port map(clock=>clock, sclr => sclrR, resetn => resetn, E=>E_IN0, D=>switches, Q=> mux0);
 f1: Multiplexer port map(A=>mux0, B=>mux1, C=>mux2, D=>mux3, SM=>SM0, BUS1=>BUS0);
 f2: my_rege port map(clock=>clock, resetn => resetn, sclr => sclrR, E=>E_R1, D=>BUS0, Q=>mux3);
@@ -95,10 +96,7 @@ f3: my_rege port map(clock=>clock, resetn => resetn, sclr => sclrR, E=>E_R0, D=>
 f4: my_rege port map(clock=>clock, resetn => resetn, sclr => sclrR, E=>E_A0, D=> BUS0, Q=>ALU0);
 f5: ALU port map(sel=>op0, A=>ALU0, B=>BUS0, G=>G0);
 f6: my_rege port map(clock=>clock, resetn => resetn, sclr => sclrR, E=>E_G0, D=>G0, Q=>mux1);
-f7: my_rege port map(clock=>clock, resetn => resetn,  sclr => sclrR, E=>E_OUT0, D=>BUS0, Q=>LEDS);
-f8: ControlCircuit port map(clock=>clock,w=>w,IR=>IR,resetn=>resetn,
-op=>op0,SM=>SM0,E_A=>E_A0,E_G=>E_G0,E_IN=>E_in0,E_OUT=>E_OUT0,
-done=>done,E_R0=>E_R0, E_R1=>E_R1);
-
+f7: my_rege port map(clock=>clock, resetn => resetn,  sclr => sclrR, E=>E_OUT0, D=>BUS0, Q=>leds0);
+f8: ControlCircuit port map(clock=>clock, w=>w, IR=>IR, resetn=>resetn, op=>op0, SM=>SM0, E_A=>E_A0, E_G=>E_G0, E_IN=>E_in0, E_OUT=>E_OUT0, done=>done, E_R0=>E_R0, E_R1=>E_R1);LEDS <= leds0;
 
 end Behavioral;
